ff7f5eb711f5e5546903e7f9e0ebc04ce84f33c4
gavin <gavin.kirby@gmail.com>
2019-07-07 22:42:16 +0100

    better variable names; remove dependence on numpy

--- README.md
+++ README.md
@@ -11,6 +11,6 @@ A suitable applicator geometry is as shown below, with wire lengths (vertices of
 The treatment time may be computed using expressions such as
 
 ```Python
->>> brachytime.treattime(sep=18, ulens=[50,55,55], llens=[70]*4, delakr=0.494, deltime='2000-11-17T12:00', midtime='2000-11-22T18:40', halflife=6379000, presc=20)
+>>> brachytime.calculate_treatment_time(separation=18, upper_lengths=[50, 55, 55], lower_lengths=[70]*4, delivery_akr=0.494, delivery_datetime='2000-11-17T12:00', mid_treatment_datetime='2000-11-22T18:40', halflife=6379000, prescribed_dose=20, printout=True)
 ```
 where the separation (the side of the triangle) and the lengths of the wires in the upper and lower planes are expressed in millimetres, the source air kerma rate at time of delivery is expressed in micrograys per hour per millimetre metre squared, the souce isotope half-life is expressed in seconds, and the prescribed dose is expressed in grays. The source delivery time and the mid-implant time must be passed as strings formatted according to ISO 8601, *i.e.* YYYY-MM-DDTHH:MM. Without loss of generality, it is assumed that the lower row of wires has at least as many wires as the upper row, and that the leftmost wire is in the lower row (since this is taken to be the origin of the coordinate system, for convenience). All wire lengths must be in the range 50 mm to 70 mm.
--- brachytime.py
+++ brachytime.py
@@ -35,91 +35,101 @@
 
 """
 
-import numpy as np
-import math as m
 from datetime import datetime as dt
-import scipy.interpolate as spi
+import math as m
+from statistics import mean
 
-__version__ = '2018-04-13T17:24:00Z'
+import scipy.interpolate as interp
 
-#Use linear interpolation for wires of intermediate length
-def interpdose(wlen, dist):
-    lens  = [50,60,70]
-    doseatlens = [calcdoserate(i, dist) for i in lens]
-    y_interp = spi.interp1d(lens, doseatlens)
-    drate = float(y_interp(wlen))
-    return drate
+__version__ = '2019-07-07T22:00:00Z'
 
-#Return the dose rate in Gy/h at a given distance (mm) from a wire of length between 50 mm and 70 mm
-#Formulae come from quadratic OLS fits of log10(doserate) to log10(dist)
-def calcdoserate(wlen, dist):
-    dist = np.log10(dist)
-    if wlen == 50:
-        drate = -0.3738*dist**2 -0.5991*dist + 0.3876 
-    elif wlen == 60:
-        drate = -0.3006*dist**2 -0.7100*dist + 0.4438
-    elif wlen == 70:
-        drate = -0.2891*dist**2 -0.6861*dist + 0.4288 
-    elif (50 < wlen < 60) or (60 < wlen < 70):
-        drate = np.log10(interpdose(wlen,np.power(10,dist)))
-    else:
-        raise ValueError('Wire length outside range [50,70]!')
-    return np.power(10,drate)
 
-#Return the treatment time in hours
-def treattime(sep, ulens, llens, delakr, deltime, midtime, halflife, presc):
+# Return the treatment time in hours.
+# If no halflife is specified, assume it's Ir-192.
+def calculate_treatment_time(
+    separation, upper_lengths, lower_lengths, delivery_akr,
+    delivery_datetime, mid_treatment_datetime,
+    prescribed_dose, halflife=6379000, printout=False):
     
-    #If no halflife is specified, assume it's Ir-192
-    if halflife == None:
-        halflife = 6379000
-    
-    #Ensure dates formatted according to ISO 8601
+    # Parse datetimes, assuming they are formatted according to ISO 8601.
     try:
-        deldate=dt.strptime(deltime, '%Y-%m-%dT%H:%M')
-        middate=dt.strptime(midtime, '%Y-%m-%dT%H:%M')        
-    except Exception as e:
+        delivery_datetime = dt.strptime(delivery_datetime, '%Y-%m-%dT%H:%M')
+        mid_treatment_datetime = dt.strptime(mid_treatment_datetime, '%Y-%m-%dT%H:%M')        
+    except Exception:
         raise ValueError('Dates and times must be formatted according to ISO 8601 (YYYY-MM-DDTHH:MM)')
 
-    if not (len(ulens) == len(llens)) and not (len(ulens) == len(llens)-1):
+    # Without loss of generality, define the lower row to be the larger.
+    if not (len(upper_lengths) == len(lower_lengths)) and not (len(upper_lengths) == len(lower_lengths)-1):
         raise ValueError('Numbers of wires in upper row must be equal to or one less than number of wires in lower row.')
-    if any(x < 50 or x > 70 for x in ulens+llens):
+    if any(x < 50 or x > 70 for x in upper_lengths + lower_lengths):
         raise ValueError('All wire lengths must be between 50 mm and 70 mm.')
-    if middate < deldate:
+    if mid_treatment_datetime < delivery_datetime:
         raise ValueError('Mid-treatment time must be after source delivery time!')
     
-    uw_coords = []
-    lw_coords = []
-    bdp_coords = []
+    # Initialise empty lists to contain coordinates
+    # of upper and lower wires, as well as basal dose points.
+    upper_coords, lower_coords, bdp_coords = [], [], []
     
-    #Compute coordinates of upper and lower wires
-    for i in range(len(ulens)):
-        uw_coords.append((sep*(i+0.5),(m.sqrt(3)/2*sep)))
+    # Compute coordinates of upper and lower wires.
+    for i in range(len(upper_lengths)):
+        upper_coords.append((separation*(i+0.5), (m.sqrt(3) / 2*separation)))
     
-    for i in range(len(llens)):
-        lw_coords.append((sep*i,0))
+    for i in range(len(lower_lengths)):
+        lower_coords.append((separation*i, 0))
     
-    #Is total number of wires (hence triangles and BDPs) odd or even?
-    if (len(llens)+len(ulens))%2 == 1:
-        for i in range(len(llens)+1):
-            bdp_coords.append((((i+1)/2)*sep,sep/(((i+1)%2+1)*m.sqrt(3))))
+    # Is total number of wires (hence triangles and BDPs) odd or even?
+    if (len(lower_lengths) + len(upper_lengths))%2 == 1:
+        for i in range(len(lower_lengths) + 1):
+            bdp_coords.append((((i+1)/2)*separation, separation/(((i+1)%2+1)*m.sqrt(3))))
             
-    elif (len(llens)+len(ulens))%2 == 0:
-        for i in range(len(llens)):
-            bdp_coords.append((((i+1)/2)*sep,sep/(((i+1)%2+1)*m.sqrt(3))))
+    elif (len(lower_lengths)+len(upper_lengths))%2 == 0:
+        for i in range(len(lower_lengths)):
+            bdp_coords.append((((i+1)/2)*separation, separation/(((i+1)%2+1)*m.sqrt(3))))
 
-    #A list of lists containing the dose rates at each BDP from each wire
+    # Populate a list of lists containing the dose rates at each BDP from each wire.
     doserates = [[] for _ in range(len(bdp_coords))]
-
-    for ix, i, jx, j in ((ix, i, jx, j) for ix, i in enumerate(bdp_coords) for jx, j in enumerate(uw_coords+lw_coords)):
-        doserates[ix].append(calcdoserate((ulens+llens)[jx],m.hypot(i[0]-j[0], i[1]-j[1])))
+    for ix, i, jx, j in ((ix, i, jx, j) for ix, i in enumerate(bdp_coords) for jx, j in enumerate(upper_coords + lower_coords)):
+        doserates[ix].append(calculate_dose_rate((upper_lengths + lower_lengths)[jx], m.hypot(i[0]-j[0], i[1]-j[1])))
       
-    #Decay-correct the source activity to mid-treatment time, then use this to compute the MBDR and thus treatment time
-    midakr = delakr*m.exp(-m.log(2)*((middate-deldate).total_seconds())/halflife)
-    mbdr = midakr*np.mean([sum(i) for i in doserates])    
-    ttime = presc/(0.85*mbdr) #The RDR is 0.85*MBDR
-    
-    return float('%.1f' % round(ttime, 1))
- 
+    # Decay-correct the source activity to mid-treatment time,
+    # then use this to compute the mean basal dose rate and thus treatment time.
+    mid_treatment_akr = delivery_akr * m.exp(-m.log(2)*((mid_treatment_datetime - delivery_datetime).total_seconds()) / halflife)
+    mbdr = mid_treatment_akr * mean([sum(i) for i in doserates])
+    treatment_time = prescribed_dose / (0.85*mbdr)  # The RDR is 0.85*MBDR.
+
+    if not printout:
+        return treatment_time
+    else:
+        print(f'The treatment time is {round(treatment_time, 1)} hours.')
+
+
+# Return the dose rate in Gy/h at a given distance (mm) from a wire of length between 50 mm and 70 mm.
+# Formulae come from quadratic OLS fits of log10(doserate) to log10(distance).
+def calculate_dose_rate(wire_length, distance):
+    distance = m.log10(distance)
+    # Case analysis of wire lengths    
+    if wire_length == 50:
+        doserate = -0.3738*distance**2 - 0.5991*distance + 0.3876 
+    elif wire_length == 60:
+        doserate = -0.3006*distance**2 - 0.7100*distance + 0.4438
+    elif wire_length == 70:
+        doserate = -0.2891*distance**2 - 0.6861*distance + 0.4288 
+    elif (50 < wire_length < 60) or (60 < wire_length < 70):
+        doserate = m.log10(interpolate_dose(wire_length, pow(10, distance)))
+    else:
+        raise ValueError('Wire length outside range [50, 70]!')
+
+    return pow(10, doserate)
+
+
+# Use linear interpolation for wires of intermediate length.
+def interpolate_dose(wire_length, distance):
+    reference_lengths  = [50, 60, 70]
+    doseatlens = [calculate_dose_rate(i, distance) for i in reference_lengths]
+    y_interp = interp.interp1d(reference_lengths, doseatlens)
+    return float(y_interp(wire_length))  # This is the interpolated doserate.  
+
+
 if __name__ == "__main__":
-    print('This is brachytime version {version}'.format(version=__version__))
+    print(f'This is brachytime version {__version__}')
     exit()
